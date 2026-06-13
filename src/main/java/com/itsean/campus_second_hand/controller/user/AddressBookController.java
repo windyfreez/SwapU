1aa87a01fed9b4678cf90cb52ecbd79ebfe85b4c
Original&Player <zhaosiyao0123@126.com>
2026-06-13 15:47:46 +0800

    修复随机排序算法bug

---
+++ src/main/java/com/itsean/campus_second_hand/controller/user/AddressBookController.java
@@ -0,0 +1,16 @@
+package com.itsean.campus_second_hand.controller.user;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/address")
+@Slf4j
+@Api(tags = "地址簿接口")
+public class AddressBookController {
+
+
+
+}
